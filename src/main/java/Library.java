8a6691d0410ed83459bfcb74805b6ed8d30f1c1b
Ankit Patel <ap991348@gmail.com>
2024-09-01 14:24:28 +0530

    Implement returning a book functionality and pass the test

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -18,4 +18,30 @@ public class Library {
         return books;
     }
 
+    public void borrowBook(String isbn) throws Exception {
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                if (book.isBorrowed()) {
+                    throw new Exception("Book is already borrowed");
+                }
+                book.setBorrowed(true);
+                return;
+            }
+        }
+        throw new Exception("Book not found");
+    }
+
+    public void returnBook(String isbn) throws Exception {
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                if (!book.isBorrowed()) {
+                    throw new Exception("Book was not borrowed");
+                }
+                book.setBorrowed(false);
+                return;
+            }
+        }
+        throw new Exception("Book not found");
+    }
+
 }

422b0e6f89f1d72bc771a02fa397c01ae962fd69
Ankit Patel <ap991348@gmail.com>
2024-09-01 14:25:13 +0530

    Write test for viewing available books

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -6,6 +6,8 @@ import main.java.Library;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 public class LibraryTest {
 
     @Test
@@ -49,6 +51,22 @@ public class LibraryTest {
         library.returnBook("1234567890");
 
         assertFalse(book.isBorrowed());
+    }
+
+    @Test
+    public void testViewAvailableBooks() throws Exception {
+        Library library = new Library();
+        Book book1 = new Book("1234567890", "The Great Gatsby", "F. Scott Fitzgerald", 1925);
+        Book book2 = new Book("0987654321", "1984", "George Orwell", 1949);
+        library.addBook(book1);
+        library.addBook(book2);
+        library.borrowBook("1234567890");
+
+        List<Book> availableBooks = library.getAvailableBooks();
+
+        assertEquals(1, availableBooks.size());
+        assertTrue(availableBooks.contains(book2));
+        assertFalse(availableBooks.contains(book1));
     }
 
 }

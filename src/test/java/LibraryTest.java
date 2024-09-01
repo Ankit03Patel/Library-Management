acca00b7f7fb0ba700338de16efe635af19b1a8c
Ankit Patel <ap991348@gmail.com>
2024-09-01 14:21:51 +0530

    Test for borrowing an unavailable book

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -27,4 +27,16 @@ public class LibraryTest {
         assertTrue(book.isBorrowed());
     }
 
+    @Test
+    public void testBorrowUnavailableBook() {
+        Library library = new Library();
+        Book book = new Book("1234567890", "The Great Gatsby", "F. Scott Fitzgerald", 1925);
+        library.addBook(book);
+
+        assertThrows(Exception.class, () -> {
+            library.borrowBook("1234567890");
+            library.borrowBook("1234567890");
+        });
+    }
+
 }

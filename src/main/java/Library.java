8cfaaa5265d8c5c25c83cc7f5ce0315d5e3dd171
Ankit Patel <ap991348@gmail.com>
2024-09-01 14:15:38 +0530

    Implement Library class to pass the add book test

---
+++ src/main/java/Library.java
@@ -0,0 +1,21 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+}
